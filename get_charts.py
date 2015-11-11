c01485e43d4b03e079e6dc615b875c63b01815cb
Vinson <vluo@stanford.edu>
2015-11-10 22:02:04 -0800

    Changing get_charts back to original form.

--- get_charts.py
+++ get_charts.py
@@ -7,7 +7,6 @@ import chart_scraper
 '''
 hot-100: top 100 songs
 billboard-200: top 200 albums
-artist-100: top 100 artists
 r-b-hip-hop-songs: top 25
 pop-songs: top 20
 country-songs: top 25
@@ -18,10 +17,10 @@ christian-songs: top 25
 '''
 
 # Note: some charts do not have data for entire range of dates
-chart_names = ['hot-100', 'billboard-200', 'artist-100', 'r-b-hip-hop-songs', 'pop-songs', 'latin-songs', \
+chart_names = ['hot-100', 'billboard-200', 'r-b-hip-hop-songs', 'pop-songs', 'latin-songs', \
   'country-songs', 'rock-songs', 'dance-electronic-songs', 'christian-songs']
 
-for chart_name in chart_names[5:6]:
+for chart_name in chart_names:
   # Grab charts from 1/1/2000 to 11/1/2015
   start_date = datetime.date(2000, 1, 1)
   end_date = datetime.date(2015, 11, 1)
