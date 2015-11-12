cdadd2c047090661e85a7fbae3acc492b200e85c
Vinson <vluo@stanford.edu>
2015-11-11 23:27:49 -0800

    Created script to get album track listing for recent billboard-200 artists.

---
+++ get_recent_artist_albums.py
@@ -0,0 +1,83 @@
+# Gets all recent artists from 2009 - present on the hot-100 and retrieves a list of their
+# albums since 2009 with track listings
+
+import pickle
+import datetime
+import spotipy
+
+build_artists = False # Whether or not to build the recent artists list from scratch
+
+start_date = datetime.date(2009, 1, 1)
+
+if build_artists:
+  charts = pickle.load(open('chart-data/billboard-200_charts.pickle', 'rb'))
+  for i in range(len(charts)):
+    if charts[i].date > start_date:
+      break
+
+  recent_charts = charts[i:]
+
+  def sanitize_artist_name(name):
+    return name
+
+  # Compile all artists
+  artists = set()
+  for chart in recent_charts:
+    for album in chart:
+      artists.add(sanitize_artist_name(album.artist))
+
+  pickle.dump(artists, open('chart-data/recent_artists.pickle', 'wb'))
+
+artists = pickle.load(open('chart-data/recent_artists.pickle', 'rb'))
+print(str(len(artists)) + ' artists loaded.')
+
+# TODO: Implement more robust track matching
+# Determine whether two track names match
+def track_match(track1, track2):
+  return track1 == track2
+
+spotify = spotipy.Spotify()
+artist_to_id = {}
+# Spotify query for artists
+for artist in artists:
+  print('Finding artist id for ' + artist)
+  results = spotify.search(q = 'artist:' + artist, type = 'artist')
+  artist_id = ''
+  # Find artist that matches name
+  for search_artist in results['artists']['items']:
+    if search_artist['name'].lower() == artist.lower():
+      artist_id = search_artist['id']
+      break
+
+  if artist_id != '':
+    artist_to_id[artist] = artist_id
+
+pickle.dump(artist_to_id, open('chart-data/artist_to_spotify_id.pickle', 'wb'))
+
+# Query for album data
+artist_to_albums = {}
+for artist, artist_id in artist_to_id.iteritems():
+  print ('Loading albums for ' + artist)
+  # If match found
+  if artist_id != '':
+    artist_to_albums[artist] = {}
+
+    results = spotify.artist_albums(artist_id, album_type='album')
+    albums = results['items']
+    while results['next']:
+      results = spotify.next(results)
+      albums.extend(results['items'])
+
+    # Find all relevant albums
+    for album in albums:
+      s_album = spotify.album(album['id'])
+      # Check to make sure release is after start_date
+      try:
+        release_date = datetime.datetime.strptime(s_album['release_date'], '%Y-%m-%d').date()
+      except:
+        continue
+      if release_date >= start_date:
+        tracks = map(lambda track: track['name'], s_album['tracks']['items'])
+        artist_to_albums[artist][s_album['name']] = (release_date, tracks)
+
+pickle.dump(artist_to_albums, open('chart-data/artist_to_albums.pickle', 'wb'))
