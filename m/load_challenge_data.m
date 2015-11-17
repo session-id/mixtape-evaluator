9400473bb1b5d90ca2ab6294c9ee915ad18a9600
Vinson <vluo@stanford.edu>
2015-11-16 21:02:41 -0800

    Converted data analysis script to cleaner n_delta format.

--- m/load_challenge_data.m
+++ m/load_challenge_data.m
@@ -8,33 +8,36 @@ if (load_data)
     fclose(fileId);
 end
 
-% Recover days that have relevant fields of information
-album_releases = ~isnan(C{104}); % IsReleaseDay
-has_fb_likes_t = ~isnan(C{6}); % Facebook_PageLikes_t
-has_youtube_views_t = ~isnan(C{101}); % YouTube_Video_Views_t
-
 % Find times when there is data for artist reaching both backwards and
 % forwards
-look_back = 14; % Number of days to shift all data backwards by
-look_forward = 14; % Number of days to shift all data forwards by
-back_buffer = -ones([look_back, 1]);
-forward_buffer = -ones([look_forward, 1]);
-artist_shift_f = [forward_buffer; C{2}(1:end-look_forward)];
-artist_shift_b = [C{2}(look_back+1:end); back_buffer];
+shift_back = 28; % Number of days to shift all data backwards by
+shift_forward = 0; % Number of days to shift all data forwards by
+back_buffer = -ones([shift_back, 1]);
+forward_buffer = -ones([shift_forward, 1]);
+artist_shift_f = [forward_buffer; C{2}(1:end-shift_forward)];
+artist_shift_b = [C{2}(shift_back+1:end); back_buffer];
 viable_days = (artist_shift_f == artist_shift_b) & (artist_shift_f == C{2});
 clear artist_shift_f
 clear artist_shift_b
 
-% Shift youtube views forward to get change in view counts
-youtube_shift_b = [C{101}(look_back+1:end); back_buffer];
-viable_days = viable_days & ~isnan(youtube_shift_b);
+% Recover days that have relevant fields of information
+album_releases = ~isnan(C{104}); % IsReleaseDay
+has_fb_likes_t = ~isnan(C{6}); % Facebook_PageLikes_t
+has_insta_followers_t = ~isnan(C{11}); % Instagram_Followers_t
 
-target_albums = find(album_releases & has_fb_likes_t & has_youtube_views_t...
+% youtube_views_delta contains total number of views (from
+% Youtube_Video_Views_t) over the next [shift_back] days from any day
+youtube_views_delta = n_delta(C{101}, shift_back);
+youtube_views_delta = [youtube_views_delta(shift_back+1:end);...
+    ones([shift_forward, 1])];
+has_youtube_delta = ~isnan(youtube_views_delta);
+
+target_albums = find(album_releases & has_fb_likes_t & has_youtube_delta...
     & viable_days);
 
 % Make linear regression features
 fb_likes_t = C{6}(target_albums);
-youtube_views_delta = youtube_shift_b(target_albums) - C{101}(target_albums);
+youtube_views_delta = youtube_views_delta(target_albums);
 
 % Simple linear regression on the log of the variables
 X = [log10(fb_likes_t), ones(size(fb_likes_t))];
@@ -49,3 +52,10 @@ hold off
 error = X * beta - y;
 error_stdev = sqrt(sum(error .^ 2) / length(y))
 abs_error = sum(abs(error)) / length(y)
+
+C_sum = zeros(size(C));
+for i=1:length(C)
+    if i ~= 1 && i ~= 4
+        C_sum(i) = sum(~isnan(C{i}));
+    end
+end
